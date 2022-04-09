b5edcde3f1cd9309d9936290209f82f1e3352c8f
christhuong <thuong.huynh@employmenthero.com>
2022-04-09 22:33:02 +0700

    Add firebase credentials

--- src/components/firebaseConfig.js
+++ src/components/firebaseConfig.js
@@ -1,9 +1,9 @@
 export default {
-  apiKey: "",
-  authDomain: "typefasterer.firebaseapp.com",
-  databaseURL: "https://typefasterer.firebaseio.com",
-  projectId: "typefasterer",
-  storageBucket: "typefasterer.appspot.com",
-  messagingSenderId: "",
-  appId: ""
-}
+  apiKey: "AIzaSyDWP2OEf9-PSrclquuQ86qNuautWgZvq-Y",
+  authDomain: "typefasterer-b6cf7.firebaseapp.com",
+  projectId: "typefasterer-b6cf7",
+  storageBucket: "typefasterer-b6cf7.appspot.com",
+  messagingSenderId: "880099538426",
+  appId: "1:880099538426:web:6925ab3e2af896723e2043",
+  measurementId: "G-VGVL2VGWTZ"
+};
