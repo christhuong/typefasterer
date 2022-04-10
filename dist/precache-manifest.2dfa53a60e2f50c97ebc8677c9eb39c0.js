076004ca035e277476d006c69c3299cde5718a83
christhuong <thuong.huynh@employmenthero.com>
2022-04-10 21:02:10 +0700

    Build and deploy with firebase

---
+++ dist/precache-manifest.2dfa53a60e2f50c97ebc8677c9eb39c0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "299d40908eec0ca29bf1",
+    "url": "/css/app.04bcbdf1.css"
+  },
+  {
+    "revision": "299d40908eec0ca29bf1",
+    "url": "/js/app.92e24363.js"
+  },
+  {
+    "revision": "bfc5a7b5d1269294b274",
+    "url": "/js/chunk-vendors.66e913eb.js"
+  },
+  {
+    "revision": "67b1ff3f256332e7d7bb8e10f0753d7d",
+    "url": "/img/kb_dark.67b1ff3f.png"
+  },
+  {
+    "revision": "ad56f371978a847917f1c55fcd12a29e",
+    "url": "/img/kb_light.ad56f371.png"
+  },
+  {
+    "revision": "0ef9ebba1dc799ae77d7e83e4afa9b9a",
+    "url": "/img/graph.0ef9ebba.png"
+  },
+  {
+    "revision": "5023f0ff865bf5b30b281d15a962585c",
+    "url": "/img/mobile_keyboard_light.5023f0ff.png"
+  },
+  {
+    "revision": "8449a3763c151f001b4087bc28d7e078",
+    "url": "/img/mobile_keyboard_dark.8449a376.png"
+  },
+  {
+    "revision": "2a86356b9a6ddd1e88f231025f228e5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38a0b5c0c7b47ec5b1088fbb10521ee9",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
